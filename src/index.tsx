49d214b112380e40f5aa2b65a86cf42f39e33cf7
Franco De Paulo <frandepaulo@yahoo.com.ar>
2022-09-29 00:29:33 -0300

    Primer commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+export { Button } from "./stories/Button";
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(<React.StrictMode></React.StrictMode>);
