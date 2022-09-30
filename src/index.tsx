301ebb681bea376c1ccfd999cbbf4169d8345854
Franco De Paulo <frandepaulo@yahoo.com.ar>
2022-09-30 00:02:15 -0300

    feat: agregamos semantic-versioning

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 // import React from "react";
 // import ReactDOM from "react-dom/client";
 
+export { Header } from "./stories/Header";
+export { Page } from "./stories/Page";
+
+export { MyLabel } from "./components/MyLabel";
 export { Button } from "./stories/Button";
 
 // const root = ReactDOM.createRoot(
